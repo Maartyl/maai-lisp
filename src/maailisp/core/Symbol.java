0c9af64107fa4d425c7349b8cedd517ad1bd5092
Maartyl <kuronomaartyl@gmail.com>
2014-12-12 01:25:42 +0100

    license headers, symbol hash

--- src/maailisp/core/Symbol.java
+++ src/maailisp/core/Symbol.java
@@ -1,9 +1,16 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2014 maartyl.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    maartyl - initial API and implementation and/or initial documentation
  */
 package maailisp.core;
+
+import java.util.Objects;
 
 /**
  *
@@ -31,10 +38,29 @@ public class Symbol extends Obj {
     return namespace == null ? name : namespace + "/" + name;
   }
 
+  @Override
+  public int hashCode() {
+    if (namespace == null) return Objects.hashCode(this.name);
+    int hash = 7;
+    hash = 83 * hash + Objects.hashCode(this.name);
+    hash = 83 * hash + Objects.hashCode(this.namespace);
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+    final Symbol other = (Symbol) obj;
+    if (!Objects.equals(this.name, other.name)) return false;
+    return Objects.equals(this.namespace, other.namespace);
+  }
+
 
   //add some method to resolve in context: keywords override to resove itself
 
   public static Symbol of(String s) {
+    if (s.length() == 1) return of(null, s);
     int slash = s.lastIndexOf('/');
     if (slash <= 0) return of(null, s);
     return of(s.substring(0, slash - 1), s.substring(slash + 1));
